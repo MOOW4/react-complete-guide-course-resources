8963c8c490180edfce000a6eaf6da11e228dff47
Christián Feliks <christian.feliks@student.tuke.sk>
2024-06-05 21:04:23 +0200

    commit 1

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,19 +1,21 @@
+import {useState, Fragment} from "react";
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts";
+import Examples from "./components/Examples";
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <main>
+                <CoreConcepts/>
+                <Examples/>
+            </main>
+        </>
+    );
 }
 
 export default App;
+
+
+// use <> <> instead of <Fragment> </Fragment> for wrapping multiple elements
---
+++ code/03 React Essentials/01-starting-project/src/components/Examples.jsx
@@ -0,0 +1,42 @@
+import TabButton from "./TabButton";
+import {EXAMPLES} from "../data.js";
+import {useState} from "react";
+import Section from "./Section";
+import Tabs from "./Tabs";
+
+export default function Examples() {
+    const [selectedTopic, setSelectedTopic] = useState("components");
+
+    function handleClick(tab) {
+        console.log(`Switch to tab: ${tab}`);
+        setSelectedTopic(tab);
+    }
+
+    let tabContent = <p>Please select a topic.</p>;
+    if (selectedTopic) {
+        tabContent = <div className="tab-content" id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+        </div>
+
+    }
+
+    return (
+        <Section title="Examples" id="examples">
+            <Tabs buttons={
+                <>
+                    <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick("components")}>Components</TabButton>
+                    <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleClick("jsx")}>JSX</TabButton>
+                    <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleClick("props")}>Props</TabButton>
+                    <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleClick("state")}>State</TabButton>
+                </>
+            }>
+                {tabContent}
+            </Tabs>
+        </Section>
+
+    );
+}
