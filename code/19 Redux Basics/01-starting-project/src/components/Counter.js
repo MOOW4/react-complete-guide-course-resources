cd9c1db1b78498cc8533dee9928acbba6f75802c
MOOW4 <christianfeliks1@gmail.com>
2024-10-04 14:05:00 +0200

    replace store with redux toolkit

--- code/19 Redux Basics/01-starting-project/src/components/Counter.js
+++ code/19 Redux Basics/01-starting-project/src/components/Counter.js
@@ -1,33 +1,35 @@
 import classes from './Counter.module.css';
 import {useDispatch, useSelector} from "react-redux";
+import {counterActions} from "../store";
 
 const Counter = () => {
   const toggleCounterHandler = () => {
-
+    dispatch(counterActions.toggle());
   };
 
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
   function handleIncrement() {
-    dispatch({type: 'increment'});
+    dispatch(counterActions.increment());
   }
   function handleDecrement() {
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   }
   function handleIncrease() {
-    dispatch({type: 'increase', amount: 5});
+    dispatch(counterActions.increase(5));
   }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>--  {counter}  --</div>
-      <div className='counter'>
+      {show &&  <div className='counter'>
         <button onClick={handleDecrement}>-</button>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleIncrease}>Increase by 5</button>
-      </div>
+      </div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
