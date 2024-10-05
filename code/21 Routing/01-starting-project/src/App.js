16ace41d140ba375f78b393567f1005be4577da6
MOOW4 <christianfeliks1@gmail.com>
2024-10-05 21:29:09 +0200

    add router

--- code/21 Routing/01-starting-project/src/App.js
+++ code/21 Routing/01-starting-project/src/App.js
@@ -1,5 +1,26 @@
+import {createBrowserRouter, RouterProvider, createRoutesFromElements, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+/*
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />,
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
+*/
+
+const router = createBrowserRouter([
+  {path: "/", element: <RootLayout />, children: [
+      {path: "/", element: <Home />},
+      {path: "/products", element: <Products />},
+    ], errorElement: <ErrorPage />},
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ code/21 Routing/01-starting-project/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import {Link} from "react-router-dom";
+
+export default function Home() {
+  return <div>
+    <h1>Home</h1>
+    <p>Welcome to the home page!</p>
+    <Link to={"/products"}>Products</Link>
+  </div>;
+}
