cd685f06e595a3c43406f6afb3286e9fbdd49a11
MOOW4 <christianfeliks1@gmail.com>
2024-10-08 16:31:42 +0200

    add page routing and loader function

---
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/EditEventPage.jsx
@@ -0,0 +1,9 @@
+
+
+export default function EditEventPage() {
+  return(
+    <div>
+      <h1>Edit Event Page</h1>
+    </div>
+  );
+}
---
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/NewEventPage.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NewEventPage() {
+  return(
+    <div>
+      <h1>New Event Page</h1>
+    </div>
+  );
+}
